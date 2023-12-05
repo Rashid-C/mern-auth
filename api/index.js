d2cc00c8a457520a633daa5c8e677e8eeeacdcff
Rashid11055 <jsoanu@gmail.com>
2023-12-06 00:39:15 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-const app=express()
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.listen(3000,()=>{
-    console.log(`"Server Listening On Port ...3000"`);
-})
+const app = express();
+
+app.listen(3000, () => {
+  console.log(`Server Listening On Port 3000`);
+});
