f05fae51a53ec1c386f9c78935aa2449742bc885
Rashid11055 <jsoanu@gmail.com>
2023-12-11 02:12:55 +0530

    complete sign up page functionality

--- api/index.js
+++ api/index.js
@@ -1,12 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
+import dotenv from "dotenv"
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
-dotenv.config();
+dotenv.config()
 
-mongoose
-  .connect(process.env.MONGO)
+mongoose.connect(process.env.MONGO)
   .then(() => {
     console.log("Connected to MongoDB Atlas");
   })
@@ -29,8 +28,8 @@ app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
-    success:false,
-    error:message,
-    statusCode:statusCode,
-  })
+    success: false,
+    error: message,
+    statusCode: statusCode,
+  });
 });
